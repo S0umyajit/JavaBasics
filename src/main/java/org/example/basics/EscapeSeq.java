4e05c9146802857d38ec1fd84a5551fe95f73cce
S0umyajit <soumyajit2k18@gmail.com>
2026-04-06 01:49:17 +0530

    Escape Sequence

---
+++ src/main/java/org/example/basics/EscapeSeq.java
@@ -0,0 +1,14 @@
+package org.example.basics;
+
+public class EscapeSeq {
+    public static void main(String[] args) {
+        System.out.println("First Pattern");
+        System.out.println("*\n*\t*\n*\t*\t*\n*\t*\t*\t*");
+
+        System.out.println("Second Pattern");
+        System.out.println("* * * *\n* * *\n* *\n*");
+
+        System.out.println("Third Pattern");
+        System.out.println("      *\n    * *\n  * * *\n* * * *");
+    }
+}
