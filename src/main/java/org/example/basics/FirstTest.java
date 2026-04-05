2f98d1557da665af23d55a73d795dac562f4f4c3
S0umyajit <soumyajit2k18@gmail.com>
2026-04-06 01:26:38 +0530

    Escape Sequence

---
+++ src/main/java/org/example/basics/FirstTest.java
@@ -0,0 +1,12 @@
+package org.example.basics;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        //Escape Sequence
+        System.out.println("Hello \"Soumyajit\" World");
+        System.out.println("Hello \'Soumyajit\' World");
+        System.out.println("Hello \nSoumyajit\nWorld");
+        System.out.println("Hello \bSoumyajit World");
+        System.out.println("Hello\tSoumyajit World");
+    }
+}
