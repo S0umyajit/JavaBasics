bc055dd59dcd4ecc6aacc3c3cacf1da51cfd5e2f
S0umyajit <soumyajit2k18@gmail.com>
2026-04-07 00:31:21 +0530

    Prac

---
+++ src/main/java/org/example/basics/CheckAllArithmeticOps.java
@@ -0,0 +1,16 @@
+package org.example.basics;
+
+public class CheckAllArithmeticOps {
+
+    public static void main(String[] args) {
+
+        int a=12;
+        int b=6;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a%b);
+    }
+}
---
+++ src/main/java/org/example/basics/ProductOfFloatingNum.java
@@ -0,0 +1,11 @@
+package org.example.basics;
+import java.util.Scanner;
+public class ProductOfFloatingNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        float a=sc.nextFloat();
+        float b=sc.nextFloat();
+        System.out.println("Product of two floating point num: "+a*b);
+    }
+}
