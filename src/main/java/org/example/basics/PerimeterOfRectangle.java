754820fb6a9a804d729af5d17d73ffc8581fc648
S0umyajit <soumyajit2k18@gmail.com>
2026-04-07 00:36:19 +0530

    Prac

---
+++ src/main/java/org/example/basics/PerimeterOfRectangle.java
@@ -0,0 +1,11 @@
+package org.example.basics;
+
+public class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        int a=12;
+        int b=13;
+        int c=12;
+        int d=19;
+        System.out.println("Perimeter OF a Rectangle: "+(a+b+c+d));
+    }
+}
