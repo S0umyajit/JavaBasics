fc422a78d1f9f64bb4a057580ce0f7082162e69e
S0umyajit <soumyajit2k18@gmail.com>
2026-04-07 01:14:48 +0530

    COmpoundInterest

--- src/main/java/org/example/basics/CompoundInterest.java
+++ src/main/java/org/example/basics/CompoundInterest.java
@@ -1,4 +1,14 @@
 package org.example.basics;
 
 public class CompoundInterest {
+
+    public static void main(String[] args) {
+
+        int p=100;
+        int rate=2;
+        int year=4;
+
+        double compoundInt=p*Math.pow((1+ (double) rate /100),year);
+        System.out.println(compoundInt);
+    }
 }
