e55d17cfea2a07108795ec81bc3cf81e7b713ced
S0umyajit <soumyajit2k18@gmail.com>
2026-04-07 01:19:34 +0530

    ConvertFahrenheitToCelcius

---
+++ src/main/java/org/example/basics/ConvertFahrenheitToCelcius.java
@@ -0,0 +1,9 @@
+package org.example.basics;
+
+public class ConvertFahrenheitToCelcius {
+    public static void main(String[] args) {
+        double far=102.3;
+        double fahrenheitToCelcius=(far-32)*(5.0/9.0);
+        System.out.println(fahrenheitToCelcius);
+    }
+}
