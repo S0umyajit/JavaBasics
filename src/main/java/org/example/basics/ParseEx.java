54770da7542f3f9f74a9d23a41f84e2194391f0d
S0umyajit <soumyajit2k18@gmail.com>
2026-04-14 01:30:29 +0530

    Java comments

---
+++ src/main/java/org/example/basics/ParseEx.java
@@ -0,0 +1,11 @@
+package org.example.basics;
+
+public class ParseEx {
+    public static void main(String[] args) {
+        String s="10";
+        String p="20";
+        //Returns a primitive int
+        int res=Integer.parseInt(s)+Integer.parseInt(p);
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/org/example/comments/Example.java
@@ -0,0 +1,17 @@
+package org.example.comments;
+
+/**
+ * This is my Example class this is for checking the various types of java comments
+ */
+public class Example {
+    public static void main(String[] args) {
+
+        //This is normal cpmment
+
+        /*
+        this is multi line comment
+        if we want to comment multiple line we can use this
+         */
+        System.out.println("hello");
+    }
+}
