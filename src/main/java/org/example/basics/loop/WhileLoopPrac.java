51624c9be0c0688ed47910dc6e77356a526098d0
S0umyajit <soumyajit2k18@gmail.com>
2026-04-14 02:00:45 +0530

    while loop

---
+++ src/main/java/org/example/basics/loop/WhileLoopPrac.java
@@ -0,0 +1,16 @@
+package org.example.basics.loop;
+
+public class WhileLoopPrac {
+    public static void main(String[] args) {
+//        int i=1;
+//        while(i<=1000){
+//            System.out.println(i);
+//            i+=1;
+//        }
+        int num=500;
+        while(num>=200){
+            System.out.println(num);
+            num-=1;
+        }
+    }
+}
